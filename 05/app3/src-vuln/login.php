22ae3cfd30067e551d53ccf81196ae014cb1a9ba
Katares <the-Secret@gmx.at>
2015-06-01 23:08:05 +0200

    Create login.php

---
+++ 05/app3/src-vuln/login.php
@@ -0,0 +1,18 @@
+<html>
+
+	<head>
+
+		<title>A4 Test/login</title>
+
+	</head>
+
+	<body>
+		<div>
+			Willkommen, bitte geben Sie Ihren Benutzernamen ein:
+		</div>
+		<form action="check.php" method="post">
+			<input type="text" name="username">
+			<input type="submit" value="senden">
+		</form>
+	</body>
+</html>
